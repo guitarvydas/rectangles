3924ffced4e831bbd14c1cd5d8d89be8d61a5e30
Paul Tarvydas <guitarvydas@gmail.com>
2021-02-16 09:19:25 -0500

    working parser

--- dot-expander.js
+++ dot-expander.js
@@ -2,21 +2,18 @@ const grammar = `
 dot_expander {
   program = (dottedIdent | anyToken)+
   dottedIdent = ident dot ident
-​
-  dot =      "[" "character" ws* "."  ws* position "]" ws*
-  ident =    "[" "symbol"    ws* text ws* position "]" ws*
-  anyToken = "[" tokenType   ws* text ws* position "]" ws*
- 
-  
+  dot =      "[" "character"     ws* "."  ws* position "]" ws*
+  ident =    "[" "symbol"  ws* text ws* position "]" ws*
+  anyToken = "[" tokenType ws* text ws* position "]" ws*
   position = ws* int ws+ int
-​
   text = encodedChar+
   encodedChar = ~ws ("A" .. "Z" | "a" .. "z" | "0" .. "9" | "-" | "_" | "." | "!" | "~" | "*" | "'" | "(" | ")" | "%")
   int = digitChar+
   digitChar = "0" .. "9" 
   tokenType = encodedChar+
   ws = " " | "\\n" | "\\t"
-}`;
+}
+`;
 
 function expand (text) {
     var ohm = require ('ohm-js');
@@ -33,7 +30,8 @@ function expand (text) {
 function main () {
     var text = getJSON("test.txt");
     var parsed = expand (text);
-    return parsed;
+    //return parsed;
+    console.log("OK");
 }
 
 
