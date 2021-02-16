00798ac878b6b5d6b241e94b8b6549f069333861
Paul Tarvydas <guitarvydas@gmail.com>
2021-02-16 09:08:07 -0500

    cosmetic

--- dot-expander.js
+++ dot-expander.js
@@ -1,13 +1,12 @@
-// npm install ohm-js
 const grammar = `
 dot_expander {
   program = (dottedIdent | anyToken)+
   dottedIdent = ident dot ident
 ​
-  dot =      "[" "character"     ws* "."  ws* position "]" ws*
-  ident =    "[" "symbol"  ws* text ws* position "]" ws*
-  anyToken = "[" tokenType ws* text ws* position "]" ws*
-  
+  dot =      "[" "character" ws* "."  ws* position "]" ws*
+  ident =    "[" "symbol"    ws* text ws* position "]" ws*
+  anyToken = "[" tokenType   ws* text ws* position "]" ws*
+ 
   
   position = ws* int ws+ int
 ​
@@ -16,7 +15,46 @@ dot_expander {
   int = digitChar+
   digitChar = "0" .. "9" 
   tokenType = encodedChar+
-  ws = " " | "\n" | "\t"
+  ws = " " | "\\n" | "\\t"
 }`;
 
+function expand (text) {
+    var ohm = require ('ohm-js');
+    var parser = ohm.grammar (grammar);
+    var result = parser.match (text);
+    if (result.succeeded ()) {
+        return result;
+    } else {
+        console.log (parser.trace (text).toString ());
+        throw "Ohm matching failed";
+    }
+}
+
+function main () {
+    var text = getJSON("test.txt");
+    var parsed = expand (text);
+    return parsed;
+}
+
+
+
+var fs = require ('fs');
+
+function getNamedFile (fname) {
+    if (fname === undefined || fname === null || fname === "-") {
+        return fs.readFileSync (0, 'utf-8');
+    } else {
+        return fs.readFileSync (fname, 'utf-8');
+    }   
+}
+
+function getJSON (fname) {
+    var s = getNamedFile (fname);
+    return s;
+    return (JSON.parse (s));
+}
+
+
+var result = main ();
+console.log(result);
 
