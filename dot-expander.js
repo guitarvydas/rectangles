8df7221bd9ddb9970d63ab5dc9cc6171bbde47ab
Paul Tarvydas <guitarvydas@gmail.com>
2021-02-17 13:20:27 -0500

    work on dot-expander
    revamped dottedIdent
    
    
    preamble and ref arrays flattened
    
    
    emit once

--- dot-expander.js
+++ dot-expander.js
@@ -46,52 +46,68 @@ function Position (line, offset) {
     this.offset = offset;
 }
 
+var preambles;
+
 function addSem (sem) {
     sem.addOperation (
 	"dot",
 	{
 	    program  : function (_1s) {  //(dottedIdent | anyToken)+
+		preambles = [];
 		var result = _1s.dot();
 		return result; },  // result is an Array of {preamble:..., tokens:...}
 	    dottedIdent  : function (_1, _2, _3) { //ident dot ident 
-		// x.y --> y(x,V) --> (prolog) insert = y(x,V_x_y), usage = V_x_y
-		var x = (_1.dot ()).tokens;
-		var y = (_3.dot ()).tokens;
-		var preambleTokenArray = [
-		    // y(x,V_x_y)
-		    new Token ("generated", y.text, x.line, x.offset),
-		    new Token ("generated", "(", x.line, x.offset),
-		    new Token ("generated", x.text, x.line, x.offset),
-		    new Token ("generated", ",", x.line, x.offset),
-		    new Token ("generated", "V_", x.line, x.offset),
-		    new Token ("generated", x.text, x.line, x.offset),
-		    new Token ("generated", "_", x.line, x.offset),
-		    new Token ("generated", y.text, x.line, x.offset),
-		    new Token ("generated", ")", x.line, x.offset)
-		];
-		var tokenArray = [
-		    // V_x_y
-		    new Token ("generated", "V_", x.line, x.offset),
-		    new Token ("generated", x.text, x.line, x.offset),
-		    new Token ("generated", "_", x.line, x.offset),
-		    new Token ("generated", y.text, x.line, x.offset)
-		];
-		return { preamble: preambleTokenArray, tokens: tokenArray };
+		// x.y --> y(x,V) --> (prolog) preamble = y(x,V_x_y), usage = V_x_y
+
+		// _1 is [preamble: [], ref: [ [symbol x lll ooo] ]
+		// _2 is [preamble: [], ref: [ [character . xxx xxx] ]
+		// _3 is [preamble: [], ref: [ [symbol y xxx xxx] ]
+
+		// newSymbol: Field_x_y
+		// originalSymbol: x
+		// originalField: y
+		// preamble: y(x,Field_x_y) --> originalField "(" originalSymbol "," newSymbol ")" 
+		// ref: [ Field_x_y ] --> [ [symbol Field_x_y lll ooo] ]
+
+		var originalSymbol = _1.dot ().ref[0];
+		var originalField = _3.dot ().ref[0];
+		var sym = originalSymbol.text;
+		var field = originalField.text;
+		var line = originalField.line;
+		var offset = originalSymbol.offset;
+		var newSymName = `Field_${sym}_${field}`;
+		var newSymbol = new Token ("symbol", newSymName, line, offset);
+		var ref = [ newSymbol ];
+		var preamble;
+		if (preambles[newSymName]) {
+		    preamble = [];
+		} else {
+		    preambles[newSymName] = true;
+		    preamble = [
+			originalField,
+			new Token ("character", "(", line, offset),
+			originalSymbol,
+			new Token ("character", ",", line, offset),
+			newSymbol,
+			new Token ("character", ")", line, offset)
+		    ];
+		};
+		return { preamble: preamble, ref: ref };
 	    },
 
 	    // tokens return {insert, text}
 	    dot  : function (_1, _2, _3, _4, _5, _6, _7, _8) {  //     "[" "character"     ws* "."  ws* position "]" ws*
 		var pos = _6.dot ();
 		var t = new Token ("character", ".", pos.line, pos.offset);
-		return {preamble: [], tokens: [t]}},
+		return {preamble: [], ref: [t]}},
 	    ident  : function (_1, _2, _3, _4, _5, _6, _7, _8) {  //   "[" "symbol"  ws* text ws* position "]" ws*
 		var pos = _6.dot ();
 		var t = new Token ("symbol", _4.dot (), pos.line, pos.offset);
-		return {preamble: [], tokens: [t]}},
+		return {preamble: [], ref: [t]}},
 	    anyToken  : function (_1, _2, _3, _4, _5, _6, _7, _8) { //"[" tokenType ws* text ws* position "]" ws*
 		var pos = _6.dot ();
 		var t = new Token (_2.dot (), _4.dot (), pos.line, pos.offset);
-		return {preamble: [], tokens: [t]}},
+		return {preamble: [], ref: [t]}},
 
 	    position  : function (_1s, _2, _3s, _4) { return new Position (_2.dot (), _4.dot ()); }, //ws* int ws+ int
 	    text  : function (_1s) { return _1s.dot ().join(''); }, //encodedChar+
@@ -135,31 +151,15 @@ function getJSON (fname) {
     return (JSON.parse (s));
 }
 
-function tokenArrayToString (a) {
-    var sArray = a.map (x => { return a.toString (); });
-    var s = sArray.join ('');
-    return s;
+function tokenArrayToStringArray (a) {
+    var sArray = a.map (token => { return token.toString (); });
+    return sArray.join ('\n');
 }
 
-function stringArrayToString (sa) {
-    var s = sa.join ('\n');
-    return s;
-}
 
 var { cst, semantics } = main ("-");
 var resultArray /*[{ preamble[], tokens[] }]*/ = semantics (cst).dot ();
-console.log ("OK");
-//console.log (resultArray);
-var preamble = stringArrayToString (resultArray.map (x => { return tokenArrayToString (x.preamble); }));
-var tokens =   stringArrayToString (resultArray.map (x => { return tokenArrayToString (x.tokens);   }));
-console.log ("*** preamble ***");
-console.log (preamble);
-console.log ("*** tokens ***");
-console.log (tokens);
-
-//var str = (tokenArray.map ((t) => {return t.toString ();})).join ('\n');
-//console.log (str);
-
-
-
-
+var preambleTokenArray = resultArray.map (x => { return x.preamble }).flat ();
+var refTokenArray = resultArray.map (x => { return x.ref }).flat ();
+console.log (tokenArrayToStringArray (preambleTokenArray));
+console.log (tokenArrayToStringArray (refTokenArray));
