8033300985fd4e459f15a002d6e21d28036d09c8
Paul Tarvydas <guitarvydas@gmail.com>
2021-02-18 15:05:48 -0500

    wip: display tokens and preamble

--- run.bash
+++ run.bash
@@ -1,6 +1,13 @@
 #!/bin/bash
 node ./insert-inserter <test.pl |\
     node ../tokens/tokenizer |\
-    node ./dot-expander |\
+    node ./dot-expander 2>_preamble.tokens |\
     ../untoken/untoken.bash |\
     ./pragma-remover.bash
+
+# currently, dot-expander outputs code ("ref") to stdout and outputs preamble to stderr
+../untoken/untoken.bash <_preamble.tokens
+
+# TODO: fold preamble into code after "%% pragma preamble insert %%"
+#   NB for PROLOG, this can't be done "globally" but must be done on a per-rule basis,
+#      since the preambles are valid only in the scope of each rule
