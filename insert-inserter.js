6841e44e9a9ad2eea4ea975855d77d0f44b727b7
Paul Tarvydas <guitarvydas@gmail.com>
2021-02-18 10:24:29 -0500

    identity grammar

--- insert-inserter.js
+++ insert-inserter.js
@@ -1,15 +1,16 @@
 const grammar = `
 PROLOG {
-  Rule =   Head ":-" body endRule
-  Head = ident ParameterList*
+  rule =   ws head ":-" body endRule
+  head = ident parameterList*
   body = bodyItem+
   bodyItem =   (~"." any) -- common
              | ("." &alnum) -- dotInSymbol
-  ident = letter alnum*
-  ParameterList = "(" (~")" any)+ ")"
-  endRule = "." space | "." end
+  ident = letter alnum* ws
+  parameterList = "(" ws (~")" any)+ ")" ws
+  endRule = "." ws end
   comment = "%" (~"\\n" any)* "\\n"
-  space += comment
+  whitespace = comment | space
+  ws = whitespace*
 }`;
 
 // Rule = %% pragma preamble clear\n${_1}${_2}%% pragma preamble insert\n${_3}${_4}` %% Head ":-" body endRule
@@ -37,20 +38,24 @@ function addSem (sem) {
     sem.addOperation (
 	"insert",
 	{
-	    Rule: function (_1, _2, _3, _4) { //Head ":-" body endRule
+	    rule: function (_01, _1, _2, _3, _4) { //Head ":-" body endRule
 		return `
+${_01.insert ()}
 %% pragma preamble clear
 ${_1.insert ()}${_2.insert ()}
 %% pragma preamble insert
 ${_3.insert ()}${_4.insert ()}`;
 	    },
-	    Head: function (_1, _2) {return `${_1.insert ()}${_2.insert ()}`; }, //ident ParameterList*
+	    head: function (_1, _2) {return `${_1.insert ()}${_2.insert ()}`; }, //ident ParameterList*
 	    body: function (_1s) {return `${_1s.insert ().join ('')}`;}, //bodyItem+
 	    bodyItem_common: function (_1) {return `${_1.insert ()}`;}, //  (~"." any) -- common
             bodyItem_dotInSymbol: function (_1,_2) {return `${_1.insert ()}`;}, // | ("." &alnum) -- dotInSymbol
-	    ident: function (_1, _2s) {return `${_1.insert ()}${_2s.insert ().join ('')}`;}, //letter alnum*
-	    ParameterList: function (_1, _2s, _3) {return `${_1.insert ()}${_2s.insert ().join ('')}${_3.insert ()}`;}, //"(" (~")" any)+ ")"
-	    endRule: function (_1, _2) {return `${_1.insert ()}${_2.insert ()}`;}, //"." space | "." end
+	    ident: function (_1, _2s, _3) {return `${_1.insert ()}${_2s.insert ().join ('')}${_3.insert ()}`;}, //letter alnum*
+	    parameterList: function (_1, _2, _3s, _4, _5) {return `${_1.insert ()}${_2.insert ()}${_3s.insert ().join ('')}${_4.insert ()}${_5.insert ()}}`;}, //"(" (~")" any)+ ")"
+	    endRule: function (_1, _2, _3) {return `${_1.insert ()}${_2.insert ()}`;},
+	    comment: function (_1, _2s, _3) { return `${_1.insert ()}${_2s.insert ().join ('')}${_3.insert ()}`; },
+	    whitespace: function (_1) { return _1.insert (); },
+	    ws: function (_1s) { return _1s.insert ().join (''); },
 	    _terminal: function () { return this.primitiveValue; }
 	}
     )
--- run.bash
+++ run.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
 node insert-inserter <test.pl
-#node insert-inserter <test.pl | ../tokens/tokenizer | dot-expander | ../untoken/untoken.bash | ./pragma-remover.bash
+#node insert-inserter <test.pl | node ../tokens/tokenizer | node ./dot-expander | ../untoken/untoken.bash
+#node insert-inserter <test.pl | node ../tokens/tokenizer | node ./dot-expander | ../untoken/untoken.bash | ./pragma-remover.bash
 #node dot-expander <testTokens.txt | ../untoken/untoken.bash
