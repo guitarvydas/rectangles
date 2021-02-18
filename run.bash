b534a1eba156c93d5a29a396261975a61555c077
Paul Tarvydas <guitarvydas@gmail.com>
2021-02-18 10:34:37 -0500

    identity preserves comments

--- run.bash
+++ run.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
-node insert-inserter <test.pl
-#node insert-inserter <test.pl | node ../tokens/tokenizer | node ./dot-expander | ../untoken/untoken.bash
+node insert-inserter <test.pl | node ../tokens/tokenizer | node ./dot-expander | ../untoken/untoken.bash
 #node insert-inserter <test.pl | node ../tokens/tokenizer | node ./dot-expander | ../untoken/untoken.bash | ./pragma-remover.bash
 #node dot-expander <testTokens.txt | ../untoken/untoken.bash
